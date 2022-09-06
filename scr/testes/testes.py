1c07a2cfae17405281bbff247e6ddeb2ed1acccd
leandrosfarias <leandro.sfarias997@gmail.com>
2022-09-05 21:06:30 -0300

    Teste Leitura de arquivo base

--- scr/testes/testes.py
+++ scr/testes/testes.py
@@ -1,4 +1,4 @@
-def teste_leitura1(path): # OBJETIVO -> VERIFICAR LEIRUTA CORRRETA DO ARQUIVO CHECANDO CABEÇALHOS
+def teste_leitura1(path):  # OBJETIVO -> VERIFICAR LEIRUTA CORRRETA DO ARQUIVO CHECANDO CABEÇALHOS
     base = open(path, 'r')
     for linha in base:
         if "states" in linha:
@@ -15,11 +15,10 @@ def teste_leitura1(path): # OBJETIVO -> VERIFICAR LEIRUTA CORRRETA DO ARQUIVO CH
     base.close()
 
 
-#teste_leitura1('.\\automatos\\DFA1.txt') #FUNCIONANDO
+teste_leitura1('.\\automatos\\DFA1.txt')  # FUNCIONANDO
 
 
-
-def teste_leitura2(path): # OBJETIVO -> OBTER TODAS AS LINHAS SEM O CABEÇALHO PARA FUTURA MANIPULAÇÃO
+def teste_leitura2(path):  # OBJETIVO -> OBTER TODAS AS LINHAS SEM O CABEÇALHO PARA FUTURA MANIPULAÇÃO
 
     linhas_sem_cabecalho = []
 
@@ -35,7 +34,55 @@ def teste_leitura2(path): # OBJETIVO -> OBTER TODAS AS LINHAS SEM O CABEÇALHO P
     return print(linhas_sem_cabecalho)
 
 
-teste_leitura2('.\\automatos\\DFA1.txt') #FUNCIONANDO
+# teste_leitura2('.\\automatos\\DFA1.txt')  # FUNCIONANDO
 
 
+def teste_leitura3(path):
+    automato = []
+    with open(path, 'r') as arquivo:
+        linhas = arquivo.readlines()
+        for linha in linhas:
+            automato.append(linha.strip('\n'))
+        arquivo.close()
+        automato = ','.join(automato).strip().split('#')
+        estados = []
+        estado_inicial = []
+        estados_aceitacao = []
+        alfabeto = []
+        transicoes = []
+        for i in range(len(automato)):
+            if 'states' in automato[i]:
+                estados = automato[i].split(',')
+                estados.remove('states')
+                if '' in estados:
+                    estados.remove('')
+            if 'initial' in automato[i]:
+                estado_inicial = automato[i].split(',')
+                estado_inicial.remove('initial')
+                if '' in estados:
+                    estados.remove('')
+            if 'accepting' in automato[i]:
+                estados_aceitacao = automato[i].split(',')
+                estados_aceitacao.remove('accepting')
+                if '' in estados:
+                    estados.remove('')
+            if 'alphabet' in automato[i]:
+                alfabeto = automato[i].split(',')
+                alfabeto.remove('alphabet')
+                if '' in estados:
+                    estados.remove('')
+            if 'transitions' in automato[i]:
+                transicoes = automato[i].split(',')
+                transicoes.remove('transitions')
+                if '' in estados:
+                    estados.remove('')
+        print(f'Estados: {estados}')
+        print(f'Estado inicial: {estado_inicial}')
+        print(f'Estados de aceitação: {estados_aceitacao}')
+        print(f'Alfabeto: {alfabeto}')
+        print(f'Transições: {transicoes}')
+        # return automato
 
+
+# teste
+teste_leitura3('automatos/DFA1.txt')
