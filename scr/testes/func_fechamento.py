58e48d75cc1bb8115078cf2a104ce1fc44463509
joaovpnt <55442431+joaovpnt@users.noreply.github.com>
2022-09-25 20:33:34 -0300

    Adição da Função fechamento na pasta de testes
    Função que verifica se existe uma transição vazia

---
+++ scr/testes/func_fechamento.py
@@ -0,0 +1,20 @@
+#from .Automato import automato
+
+def e_fechamento(automato, estado):
+  alcanca = [estado]
+  for transicao in automato.matriz_transicoes:
+    if estado == transicao[0] and "$" in transicao[1]:
+      alcanca.append(transicao[2])
+      estado_aux = transicao[2]
+
+      for transicao_ in automato.matriz_transicoes:
+        if estado_aux == transicao_[0] and "$" in transicao_[1]:
+          if transicao_[2] not in alcanca:
+            alcanca.append(transicao_[2])
+            estado_aux = transicao_[2]
+
+
+  return alcanca
+
+
+e_fechamento(automato, automato.estado_inicial[0])
