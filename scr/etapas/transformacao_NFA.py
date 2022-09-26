a058bff615a79f90025321418d1852959de8992b
Marcelo Cardoso <marcelosouza7r@gmail.com>
2022-09-26 15:30:19 -0300

    Criação da tabela de transição para NFA, e_fechamento no codigo principal

--- scr/etapas/transformacao_NFA.py
+++ scr/etapas/transformacao_NFA.py
@@ -1,3 +1,37 @@
+def tabela_transicao(automato, tabela, conjunto):
+    if len(tabela) == 0:
+
+        for s in automato.alfabeto:
+            tabela[(tuple(conjunto), s)] = []
+            for t in automato.matriz_transicoes:
+                for estado in range(len(tuple(conjunto))):
+                    if tuple(conjunto)[estado] in t[0] and s in t[1]:
+                        if tuple(conjunto) not in tabela[(tuple(conjunto), s)]:
+                            tabela[(tuple(conjunto), s)].append(t[-1])
+
+        for conjunto_ in tabela.values():
+            if "".join(conjunto_) not in automato.estados:
+                return tabela_transicao(automato, tabela, conjunto_)
+
+    else:
+        if "".join(conjunto) not in automato.estados:
+            automato.estados.append("".join(conjunto))
+
+            for s in automato.alfabeto:
+                tabela[(tuple(conjunto), s)] = []
+                for t in automato.matriz_transicoes:
+                    for estado in range(len(tuple(conjunto))):
+                        if tuple(conjunto)[estado] in t[0] and s in t[1]:
+                            if tuple(conjunto) not in tabela[(tuple(conjunto), s)]:
+                                tabela[(tuple(conjunto), s)].append(t[-1])
+
+        for conjunto_ in tabela.values():
+            if "".join(conjunto_) not in automato.estados:
+                return tabela_transicao(automato, tabela, conjunto_)
+
+        else:
+            return tabela
+
 
 
 def transformacao_NFA():
--- scr/etapas/transformacao_eNFA.py
+++ scr/etapas/transformacao_eNFA.py
@@ -1,3 +1,19 @@
+def e_fechamento(automato, estado):
+  alcanca = [estado]
+  for transicao in automato.matriz_transicoes:
+    if estado == transicao[0] and "$" in transicao[1]:
+      alcanca.append(transicao[2])
+      estado_aux = transicao[2]
+
+      for transicao_ in automato.matriz_transicoes:
+        if estado_aux == transicao_[0] and "$" in transicao_[1]:
+          if transicao_[2] not in alcanca:
+            alcanca.append(transicao_[2])
+            estado_aux = transicao_[2]
+
+
+  return alcanca
+
 
 
 def transformacao_eNFA():
